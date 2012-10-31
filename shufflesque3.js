b0ece6084b3ac483b0957cb98c6c4c8a15531cb2
Steven La <mrstevenla@gmail.com>
2012-10-30 21:16:29 -0700

    animations for new track info works

--- shufflesque3.js
+++ shufflesque3.js
@@ -7,11 +7,45 @@ function themeReady() {
 function playerUpdate() {
   var ratio = Player.playerPosition() / Player.currentTrack().length;
   var percent = (ratio * 100) + '%';
-  $('#progress-bar').width(percent);
+  $('.current .progress-bar').width(percent);
 }
 
 function trackUpdate(track) {
-  $('#name').text(track.title);
+  var newTrack = $('#base').clone().appendTo('body');
+  newTrack.removeAttr('id');
+  var oldTrack = $('.info.current');
+
+  // Populate new track's data
+  var $name = newTrack.find('.name');
+  $name.text(track.title);
+  newTrack.find('.artist').text(track.artist);
+  newTrack.find('.album').text(track.album);
+
+  // Set current track
+  newTrack.addClass('current');
+  oldTrack.removeClass('current');
+
+  // Set progress bar width to max of title or meta info
+  var nameWidth = $name.width();
+  var metaWidth = newTrack.find('.meta-info').width();
+  var maxWidth = Math.max(nameWidth, metaWidth);
+  newTrack.find('.progress-wrap').width(maxWidth);
+
+  // Remove track when after animation finishes
+  var t = setTimeout(function () {
+    oldTrack.remove();
+  }, 1000);
+}
+
+function artworkUpdate(artworkURL) {
+  var $main = $('.main');
+  if (artworkURL == "") {
+    $main.hide();
+  }
+  else {
+    $main.show();
+    $main.attr('src', artworkURL);
+  }
 }
 
 // Center clicking stuff
@@ -46,4 +80,6 @@ function artAction() {
   else {
     Player.previousTrack();
   }
+  // Reset clickCount
+  clickCount = 0;
 }
